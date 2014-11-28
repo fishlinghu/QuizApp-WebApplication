0622cfa87941428162c716dc37e25e023964a93d
Rasmus Valling <rv5g11@soton.ac.uk>
2014-11-28 10:14:50 +0000

    bugfixes to quiz game model

--- quizapp/quizapp/models/game.py
+++ quizapp/quizapp/models/game.py
@@ -9,13 +9,13 @@ class Game(db.Model):
 	a_ID = db.IntegerProperty()
 	b_ID = db.IntegerProperty()
 	#used to save a list of question ID for the questions that should appear in this game 
-	question_set = db.ListProperty()
+	question_set = db.ListProperty(int)
 	#record the answers of each player every round
-	a_ans_list = db.ListProperty()
-	b_ans_list = db.ListProperty()
+	a_ans_list = db.ListProperty(bool)
+	b_ans_list = db.ListProperty(bool)
 	#total socres of two players
 	a_score = db.IntegerProperty()
 	b_score = db.IntegerProperty()
 	#record the scores each player got in every round
-	a_score_list = db.ListProperty()
-	b_score_list = db.ListProperty()
+	a_score_list = db.ListProperty(int)
+	b_score_list = db.ListProperty(int)
