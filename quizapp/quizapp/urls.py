259f63f680259841323a1e1ed1e1f2852b275ac6
Rasmus Valling <rv5g11@soton.ac.uk>
2014-12-05 13:33:02 +0000

    added single question addition route

--- quizapp/quizapp/urls.py
+++ quizapp/quizapp/urls.py
@@ -15,6 +15,7 @@ from quizapp.views.message import MessageHandler
 from quizapp.views.friend import FriendHandler
 from quizapp.views.setting import SettingHandler
 from quizapp.views.add_questions import AddQuestionsHandler
+from quizapp.views.add_question import AddQuestionHandler
 
 routes = [
     ('/', HomepageHandler),
@@ -34,5 +35,6 @@ routes = [
     #('/info', InfoHandler),  # Show your personal information, maybe everyone can view other people's profile
     ('/setting', SettingHandler), # Change your informations here, such as account, password, name, etc. 
     ('/friend', FriendHandler), # Show your friends list, you can also add/delete friend here
-    ('/add_questions', AddQuestionsHandler)
+    ('/add_questions', AddQuestionsHandler),
+    ('/add_question', AddQuestionHandler)
 ]
