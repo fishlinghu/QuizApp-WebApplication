a79735782ac3551e763cd2d44957fa6f4a71c5ad
Rasmus Valling <rv5g11@soton.ac.uk>
2014-12-05 13:32:45 +0000

    fixed indentation errors

--- quizapp/quizapp/models/question.py
+++ quizapp/quizapp/models/question.py
@@ -2,9 +2,9 @@ from google.appengine.ext import db
 
 class Question(db.Model):
     question = db.StringProperty()
-	description = db.StringProperty()
-	correct_ans = db.StringProperty()
-	wrong_ans = db.StringListProperty()
-	wiki_link = db.StringProperty()
+    description = db.StringProperty()
+    correct_ans = db.StringProperty()
+    wrong_ans = db.StringListProperty()
+    wiki_link = db.StringProperty()
     img_link = db.StringProperty()
-	topic_ID = db.IntegerProperty()
+    topic_ID = db.IntegerProperty()
