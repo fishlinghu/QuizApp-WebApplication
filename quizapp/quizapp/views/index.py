762ffaa4038d4f98de7e25a443a3c8c59eb9c134
julian-yon <julian.yon@hotmail.co.uk>
2014-12-05 16:17:29 +0000

    Updates to Quiz & Display of Player Names
    Added code to base HTML templates to display the names of both player in
    the appropriate places. Moved the class QuizUpdater from the 'quiz.py'
    file to the 'submit.py'file.

--- quizapp/quizapp/views/index.py
+++ quizapp/quizapp/views/index.py
@@ -3,6 +3,7 @@
 
 from views import Handler
 from quizapp.models.question import Question
+from quizapp.models.player import Player
 from google.appengine.ext import db
 
 class IndexHandler(Handler):
@@ -10,8 +11,10 @@ class IndexHandler(Handler):
         self.render("index.html", **kw)
 
     def get(self):
+        playerID = self.session['QUIZAPP_USER']
+        player = Player.get_by_id(playerID)  
         self.response.headers['Content-Type'] = 'text/html'
-        self.render_homepage()
+        self.render_homepage(name = player.account)
         """
         question1 = Question(
                             question_ID = 1,
