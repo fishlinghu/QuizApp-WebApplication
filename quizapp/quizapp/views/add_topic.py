3ee101dd36098d58a41ff1b4be0c068679104fa0
no2g12 <no2g12@soton.ac.uk>
2014-12-27 13:45:37 +0000

    redone the topic.py
    a topic id does not need to be entered manually anymore

--- quizapp/quizapp/views/add_topic.py
+++ quizapp/quizapp/views/add_topic.py
@@ -22,7 +22,7 @@ class AddTopicHandler(Handler):
         topic = Topic(
             description = description,
             name = name,
-            topicID = topicID.replace(" ", "")
+            topicID = name.replace(" ", "")
         )
         topic.put()
         self.render_add_topic(success="New topic " + name + " added.")
