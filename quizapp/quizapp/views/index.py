5c2307dfff46184dc8ae3c2fd8b537b56ac0fa64
no2g12 <no2g12@soton.ac.uk>
2014-12-27 00:43:41 +0000

    updated index page so topics would show up

--- quizapp/quizapp/views/index.py
+++ quizapp/quizapp/views/index.py
@@ -4,9 +4,11 @@
 from views import Handler
 from quizapp.models.question import Question
 from quizapp.models.player import Player
+from quizapp.models.topic import Topic 
 from google.appengine.ext import db
 
 class IndexHandler(Handler):
+    topics = Topic.all()
     def render_homepage(self, **kw):
         if (self.check_clearance()):
             self.render("index.html", **kw)
@@ -15,7 +17,7 @@ class IndexHandler(Handler):
         playerID = self.session['QUIZAPP_USER']
         player = Player.get_by_id(playerID)  
         self.response.headers['Content-Type'] = 'text/html'
-        self.render_homepage(name = player.account)
+        self.render_homepage(name = player.account, topics=self.topics)
         """
         question1 = Question(
                             question_ID = 1,
