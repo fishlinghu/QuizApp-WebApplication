0970539f36a804ebce570438e17118299a9aa6b7
no2g12 <no2g12@soton.ac.uk>
2015-01-01 21:39:21 +0000

    moved code
    moved some of the code from the quiz.py into the wait.py

--- quizapp/quizapp/views/quiz.py
+++ quizapp/quizapp/views/quiz.py
@@ -16,7 +16,7 @@ class QuizHandler(Handler):
     def render_quiz(self, **kw):
         self.render("quiz.html", **kw)
 
-    def get(self, topic):      
+    def get(self, topic):
         self.response.headers['Content-Type'] = 'text/html'
         user = self.session.get('QUIZAPP_USER')
         quiz_key = self.session.get('QUIZAPP_QUIZ')
@@ -26,50 +26,7 @@ class QuizHandler(Handler):
         topicID = q.get().key().id()
         
         if user:
-            if not quiz_key:
-                q = db.Query(Game)
-                q.filter('b_ID =', None)
-                q.filter('a_ID !=', user)
-                q.filter('topic_ID =', topicID) 
-                quiz = q.get()
-                
-                if not quiz:
-                    q = db.Query(Question)
-                    q.filter('topic_ID =', topicID)
-        
-                    questionRange = q.count()
-                    questions = []
-                    
-                    for i in range(5):
-                        #Generate a random integer which dictates the question at that position
-                        questionNumber = random.randint(0, questionRange - 1)
-                        #Get the question from the datastore using the randomly generated integer
-                        question = q.get(offset = questionNumber)
-                        #Append question ID
-                        questions.append(question.key().id())
-                        
-                    quiz = Game(
-                                a_ID = user,
-                                question_set = questions,
-                                a_ans_list = [],
-                                b_ans_list = [],
-                                a_score = 0,
-                                b_score = 0,
-                                a_score_list = [],
-                                b_score_list = [],
-                                topic_ID = topicID
-                                )
-                    quiz.put()
-                    quiz_key = quiz.key().id()
-                    self.session['QUIZAPP_QUIZ'] = quiz_key
-                else:
-                    quiz.b_ID = user
-                    quiz.put()
-                    quiz_key = quiz.key().id()
-                    self.session['QUIZAPP_QUIZ'] = quiz_key
-            
-            else:
-                quiz = Game.get_by_id(int(quiz_key))             
+            quiz = Game.get_by_id(int(quiz_key))             
             
             if quiz:
                 token = channel.create_channel(str(user) + str(quiz_key))
