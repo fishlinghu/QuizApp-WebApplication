bdba9f10a4b6c9ef479a0dee78e5f5ca4528f174
no2g12 <no2g12@soton.ac.uk>
2015-01-01 22:09:33 +0000

    added comments to the wait.py code
    hopefully this will help you understand the game better

--- quizapp/quizapp/views/wait.py
+++ quizapp/quizapp/views/wait.py
@@ -21,6 +21,7 @@ class WaitHandler(Handler):
         user = self.session.get('QUIZAPP_USER')
         quiz_key = self.session.get('QUIZAPP_QUIZ')
         
+        #Search database for the correct topic
         q = db.Query(Topic)
         q.filter('topicID =', topic)
         topicID = q.get().key().id()
@@ -28,6 +29,8 @@ class WaitHandler(Handler):
         if user:
             playerID = self.session['QUIZAPP_USER']
             player = Player.get_by_id(playerID)
+            
+            #if no quiz_key (game) is assigned to the session then search the database
             if not quiz_key:
                 q = db.Query(Game)
                 q.filter('b_ID =', None)
@@ -35,6 +38,7 @@ class WaitHandler(Handler):
                 q.filter('topic_ID =', topicID) 
                 quiz = q.get()
                 
+                #if no game is found in the database, create a new game
                 if not quiz:
                     q = db.Query(Question)
                     q.filter('topic_ID =', topicID)
@@ -65,17 +69,25 @@ class WaitHandler(Handler):
                     quiz_key = quiz.key().id()
                     self.session['QUIZAPP_QUIZ'] = quiz_key
                     self.render_wait(name = player.account, topic = topic)
+                
+                #if a game was found in the database, add user to the player b slot and play the game
                 else:
                     quiz.b_ID = user
                     quiz.put()
                     quiz_key = quiz.key().id()
                     self.session['QUIZAPP_QUIZ'] = quiz_key
                     self.redirect("/quiz/" + topic + "/")
+            
+            #if quiz_key (game) is assigned to the session, check if the game has an opponant
             else:
                 quiz = Game.get_by_id(int(quiz_key))
-                opponant = quiz.b_ID
-                if not opponant:
+                opponent = quiz.b_ID
+                
+                #if no opponant is found, stay on the wait page
+                if not opponent:
                     self.render_wait(name = player.account, topic = topic)
+                
+                #if opponant is found then play the game
                 else:
                     self.redirect("/quiz/" + topic + "/")
         else:
