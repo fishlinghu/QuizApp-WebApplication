89c9917a67207a93e31359bfa1018b9f6d3ea381
no2g12 <no2g12@soton.ac.uk>
2015-01-04 03:54:45 +0000

    submit.py alter
    changed the game mechanics to try and avoid the multi click bug

--- quizapp/quizapp/views/submit.py
+++ quizapp/quizapp/views/submit.py
@@ -6,7 +6,6 @@ import json
 import random
 from google.appengine.ext import db
 from quizapp.models.game import Game
-from quizapp.models.player import Player
 from quizapp.models.question import Question
 from google.appengine.api import channel
 
@@ -14,7 +13,7 @@ class QuizUpdater(Game):
     quiz = None
     
     def __init__(self, quiz):
-        self.quiz  = quiz
+        self.quiz = quiz
         
     def get_quiz(self):
         return quiz
@@ -43,38 +42,27 @@ class SubmitHandler(Handler):
         self.response.headers['Content-Type'] = 'text/html'
         user = int(self.request.get('user'))
         quiz_key = int(self.request.get('quiz_key'))  
-        answer = True
-        score = int(self.request.get('score'))
-        
-        
+        answer = self.request.get('answer')
+
         #Get the quiz being played by the player
         quiz = Game.get_by_id(quiz_key)
-        
+            
         #Update quiz based on player
         if user == quiz.a_ID:
             quiz.a_ans_list.append(answer)
-            quiz.a_score_list.append(score)
-            quiz.a_score += score
-        elif user == quiz.b_ID:
-            quiz.b_ans_list.append(answer)
-            quiz.b_score_list.append(score)
-            quiz.b_score += score
-            
-        quiz.put()
-            
-        #Check if a round has ended
-        if len(quiz.a_ans_list) == len(quiz.b_ans_list):
+            quiz.put()
+            #Check if a round has ended for the player
             if len(quiz.a_ans_list) < 5:
                 #Send message containing next question
                 questionNumber = len(quiz.a_ans_list)
                 questionNumber = quiz.question_set[questionNumber]
                 nextQuestion = Question.get_by_id(questionNumber)
-            
+                            
                 #Shuffle answers to question
                 answers = nextQuestion.wrong_ans
                 answers.append(nextQuestion.correct_ans)
                 random.shuffle(answers)
-                
+                    
                 for answer in answers:
                     answer = answer.title()
                 
@@ -83,12 +71,37 @@ class SubmitHandler(Handler):
                                   'img_link' : nextQuestion.img_link,
                                   'answers' : answers
                                   }
-            
+                
                 questionUpdate = json.dumps(questionUpdate)
                 channel.send_message(str(quiz.a_ID) + str(quiz_key), questionUpdate)
+        elif user == quiz.b_ID:
+            quiz.b_ans_list.append(answer)
+            quiz.put()
+            #Check if a round has ended for the player
+            if len(quiz.b_ans_list) < 5:
+                #Send message containing next question
+                questionNumber = len(quiz.b_ans_list)
+                questionNumber = quiz.question_set[questionNumber]
+                nextQuestion = Question.get_by_id(questionNumber)
+                            
+                #Shuffle answers to question
+                answers = nextQuestion.wrong_ans
+                answers.append(nextQuestion.correct_ans)
+                random.shuffle(answers)
+                    
+                for answer in answers:
+                    answer = answer.title()
+                
+                questionUpdate = {
+                                  'question' : nextQuestion.question,
+                                  'img_link' : nextQuestion.img_link,
+                                  'answers' : answers
+                                  }
+                
+                questionUpdate = json.dumps(questionUpdate)
                 channel.send_message(str(quiz.b_ID) + str(quiz_key), questionUpdate)
-            else:
-                player = Player.get_by_id(user)
-                player.game_history.append(quiz_key)
-                db.put(player)
-                self.redirect('/results')
+        
+        #Check if quiz has ended
+        #if len(quiz.a_ans_list) == len(quiz.b_ans_list):
+            #if len(quiz.a_ans_list) == 5:
+        self.redirect('/results')
