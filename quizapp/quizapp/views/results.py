c112f997df9b99dba134e45d0d3d3e3bee2ca4df
no2g12 <no2g12@soton.ac.uk>
2015-01-04 03:51:10 +0000

    results.py now calculates the score
    score is calculated in results.py

--- quizapp/quizapp/views/results.py
+++ quizapp/quizapp/views/results.py
@@ -18,36 +18,63 @@ class ResultsHandler(Handler):
 	def get(self):
 		# self.render_result()
 		user = self.session.get('QUIZAPP_USER')
+		quiz_key = self.session.get('QUIZAPP_QUIZ')
+		
 		if user: 
 			# Should add the game ID(key().id()) to the end of player's "game_history" list when a quiz start
 			# So this should be done in quiz.py
 
 			# get the user
 			player = Player.get_by_id(user)
-
-			game = Game.get_by_id(player.game_history[-1])
+			game = Game.get_by_id(int(quiz_key))     
+			
+			gameAnswers = game.question_answers
+			playerAAnswers = game.a_ans_list
+			playerBAnswers = game.b_ans_list
+			playerAScore = 0
+			playerBScore = 0
+			
+			for i in range(5):
+				answerPosition = i-1
+				correctAnswer = gameAnswers[answerPosition]
+				aAnswer = playerAAnswers[answerPosition]
+				bAnswer = playerBAnswers[answerPosition]
+				
+				if aAnswer == correctAnswer:
+					playerAScore = playerAScore + 100
+					game.a_score_list.append(100)
+				else:
+					game.a_score_list.append(0)
+				
+				if bAnswer == correctAnswer:
+					playerBScore = playerBScore + 100
+					game.b_score_list.append(100)
+				else:
+					game.b_score_list.append(0)
+				
+				game.put()
 			
 			if game.a_ID == player.key().id():
 				# user's ID == a_ID
-				your_score = game.a_score
-				opp_score = game.b_score
+				your_score = playerAScore
+				opp_score = playerBScore
 			else:
 				# user's ID == b_ID
-				your_score = game.b_score
-				opp_score = game.a_score
+				your_score = playerBScore
+				opp_score = playerAScore
 
 			# Check who wins
 			if your_score > opp_score:
 				# Show you win
-				win_or_lose = "You Win!!"
+				win_or_lose = "You Win!"
 				w_o_l = 2
 			elif your_score < opp_score:
 				# Show you lose
-				win_or_lose = "You Lose!!"
+				win_or_lose = "You Lose!"
 				w_o_l = 0
 			else:
 				# Even
-				win_or_lose = "Even Game"
+				win_or_lose = "The game is a tie!"
 				w_o_l = 1
 
 			# Need a function of score/win/lose to calculate the experience an user get
@@ -55,17 +82,18 @@ class ResultsHandler(Handler):
 			if player.experience:
 				player.experience = player.experience + exp
 			else:
-				player.experience = 0
-				player.experience = player.experience + exp
+				player.experience = exp
 
 			# Can set how much experiences you need to level-up here
 			if player.experience >= 5000:
 				# level up
-				player.experience = player.experience - 5000
-				player.level = player.level + 1
+				playerEXP = player.experience - 5000
+				player.experience = playerEXP
+				playerLevel = player.level + 1
+				player.level = playerLevel
 
 			# Update the player entity
-			db.put(player)
+			player.put()
 
 			player_a = Player.get_by_id(game.a_ID)
 			player_b = Player.get_by_id(game.b_ID)
